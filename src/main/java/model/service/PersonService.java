00f1658856b81acaa133280daa8eb87e10c5d9e0
BabakOveisi <babakoveisi1375@gmail.com>
2025-08-22 17:47:48 +0330

    Add Service Interface. Add PersonService.

---
+++ src/main/java/model/service/PersonService.java
@@ -0,0 +1,80 @@
+package model.service;
+
+import controller.exception.AgeIsNotEnoughException;
+import controller.exception.PersonNotFoundByFirstNameAndLastNameException;
+import controller.exception.PersonNotFoundByIdException;
+import lombok.Getter;
+import model.entity.Person;
+import model.repository.PersonRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class PersonService implements Service<Person, Integer> {
+    @Getter
+    private final static PersonService service = new PersonService();
+
+    private PersonService() {
+    }
+
+    @Override
+    public void save(Person person) throws Exception {
+        try (PersonRepository personRepository = new PersonRepository()) {
+            if (person.getBirthDate().isAfter(LocalDate.of(2010, 1, 1)) && person.getRole().name().equals("manager")) {
+                throw new AgeIsNotEnoughException();
+            } else {
+                personRepository.save(person);
+            }
+        }
+    }
+
+    @Override
+    public void update(Person person) throws Exception {
+        try (PersonRepository personRepository = new PersonRepository()) {
+            if (person.getBirthDate().isAfter(LocalDate.of(2010, 1, 1)) && person.getRole().name().equals("manager")) {
+                throw new AgeIsNotEnoughException();
+            } else {
+                personRepository.update(person);
+            }
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) throws Exception {
+        try (PersonRepository personRepository = new PersonRepository()) {
+            if (personRepository.findById(id) != null) {
+                personRepository.deleteById(id);
+            }
+        }
+    }
+
+    @Override
+    public List<Person> findAll() throws Exception {
+        try (PersonRepository personRepository = new PersonRepository()) {
+            return personRepository.findAll();
+        }
+    }
+
+    @Override
+    public Person findById(Integer id) throws Exception {
+        try (PersonRepository personRepository = new PersonRepository()) {
+            Person person = personRepository.findById(id);
+            if (person == null) {
+                throw new PersonNotFoundByIdException(id);
+            } else {
+                return person;
+            }
+        }
+    }
+
+    public List<Person> findByFirstNameAndLastName(String firstName, String lastName) throws Exception {
+        try (PersonRepository personRepository = new PersonRepository()) {
+            List<Person> personList = personRepository.findByFirstNameAndLastName(firstName, lastName);
+            if (personList.isEmpty()) {
+                throw new PersonNotFoundByFirstNameAndLastNameException(firstName, lastName);
+            } else {
+                return personList;
+            }
+        }
+    }
+}
--- src/main/java/model/service/Service.java
+++ src/main/java/model/service/Service.java
@@ -1,14 +1,15 @@
 package model.service;
 
-import model.entity.Person;
-
-import java.sql.SQLException;
 import java.util.List;
 
 public interface Service<O, I> {
-    void save(O o) throws SQLException;
-    void update(O o) throws SQLException;
-    void deleteById(Integer id) throws SQLException;
-    List<O> findAll() throws SQLException;
-    O findById(Integer id) throws SQLException;
+    void save(O o) throws Exception;
+
+    void update(O o) throws Exception;
+
+    void deleteById(Integer id) throws Exception;
+
+    List<O> findAll() throws Exception;
+
+    O findById(Integer id) throws Exception;
 }
