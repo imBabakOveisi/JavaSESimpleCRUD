b0a72754eb0673a5c99d4c1ff1d6c3215acca5a6
BabakOveisi <babakoveisi1375@gmail.com>
2025-08-22 15:54:28 +0330

    Correct Repository interface

--- src/main/java/model/repository/Repository.java
+++ src/main/java/model/repository/Repository.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface Repository<O, I> {
-    public void  save(O o) throws SQLException;
-    public void  update(O o) throws SQLException;
-    public void  deleteById(I id) throws SQLException;
-    public List<O> findAll() throws SQLException;
-    public O findById(I id) throws SQLException;
+    void  save(O o) throws SQLException;
+    void  update(O o) throws SQLException;
+    void  deleteById(I id) throws SQLException;
+    List<O> findAll() throws SQLException;
+    O findById(I id) throws SQLException;
 }
