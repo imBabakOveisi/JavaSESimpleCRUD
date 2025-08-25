98331f309d45afea9cdaea9d6ffcbe2546ef2c4d
BabakOveisi <babakoveisi1375@gmail.com>
2025-08-25 12:41:14 +0330

    Add Education entity to database_creator.sql Add educations table. Add education_seq sequence.

--- src/main/java/model/repository/database_creator.sql
+++ src/main/java/model/repository/database_creator.sql
@@ -8,3 +8,17 @@ create table persons (
 );
 
 create sequence person_seq start with 1 increment by 1;
+
+--
+
+create table educations (
+    education_id number primary key ,
+    person_id number references persons,
+    university nvarchar2(50) not null ,
+    education_grade nvarchar2(30) not null ,
+    average number(4, 2) not null ,
+    start_date date not null ,
+    end_date date not null
+);
+
+create sequence education_seq start with 1 increment by 1;
